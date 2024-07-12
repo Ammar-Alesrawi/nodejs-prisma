97912f05348ea6910c93b45b49d1003bbfaaa81b
Ammar <esrawiaammar22@gmail.com>
2024-07-13 00:17:21 +0300

    res.send

--- routes/api/get.js
+++ routes/api/get.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 // api to get all users
 router.get('/', async (req, res) => {
   try {
-    send.json("hello world")
+    res.send("hello world")
   } catch (err) {
     res.status(500).json({ error: 'Something went wrong' });
   }

4e05bd5934b0479aedb40b62787513c2134686dc
Ammar <esrawiaammar22@gmail.com>
2024-07-13 00:16:43 +0300

    update get api

--- index.js
+++ index.js
@@ -2,28 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes/api/index');
-const prisma = require('./prisma/prisma');
 const app = express();
 
 
 app.use(express.json());
 app.use(morgan('dev'));
-
-async function seedDatabase() {
-  const existingUsers = await prisma.user.findMany();
-  if (existingUsers.length === 0) {
-    console.log('Seeding database...');
-    const seed = await require('./prisma/seed')();
-    await seed();
-  }
-}
-seedDatabase()
-  .then(() => {
-    console.log('Database seeding completed.');
-  })
-  .catch(err => {
-    console.error('Error seeding database:', err);
-  });
 
   // ROUTES//
 app.use(routes);
--- routes/api/get.js
+++ routes/api/get.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const prisma = require('../../prisma/prisma');
 
 // api to get all users
 router.get('/', async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    send.json("hello world")
   } catch (err) {
     res.status(500).json({ error: 'Something went wrong' });
   }
